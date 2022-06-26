4decb166d7f5156cfb200a7822878757e76e19dc
ezov86 <mahintim2@gmail.com>
2022-06-26 08:47:34 +0000

    Изменены шаблоны .circ файла, скрипт сборки доработан для последующего расширения

--- build.py
+++ build.py
@@ -1,41 +1,76 @@
+#!/usr/bin/python
 import os
+import sys
 
 
-class Logisim:
-    MICROCODE_REL_PATH = 'mc.bin'
-    JAR_REL_PATH = 'logisim-comps/target/k818-logisim-comps-1.0-SNAPSHOT.jar'
+class Module:
+    errors_count = 0
+    total_count = 0
+    done_count = 0
+
+    def try_exec(self, func: str) -> bool:
+        if hasattr(self, func):
+            Module.total_count += 1
+            try:
+                print(f"- Running '{func}'...")
+                getattr(self, func)()
+                Module.done_count += 1
+                print(f"- Done")
+            except Exception as e:
+                Module.errors_count += 1
+                print(f'- Error: {e}')
+                print(e.__traceback__)
+
+            return True
+        else:
+            return False
+
+    @classmethod
+    def report(cls):
+        print(f'[ERRORS: {cls.errors_count}] [OK: {cls.done_count}] [TOTAL: {cls.total_count}]')
+
+
+class Logisim(Module):
+    CIRC_NAME = 'cpu.circ'
+    CIRCTMPL_NAME = 'cpu.circtmpl'
 
     def __init__(self, cd_path: str):
         self.cd_path = cd_path
 
-    def template(self, circ_path: str, circtmpl_path: str):
+    def tmpl(self):
         """
         Создание .circtmpl файла из .circ (все пути к файлам заменены на шаблоны).
         """
-        s = open(circ_path, 'r').read()
+        s = open(self.CIRC_NAME, 'r').read()
+        open(self.CIRCTMPL_NAME, 'w+').write(s.replace(self.cd_path, '%CD_PATH%'))
 
-        # Замена пути к микрокоду.
-        s = s.replace(f'{self.cd_path}/{self.MICROCODE_REL_PATH}', '%MICROCODE_PATH%')
-        # Замена пути к JAR компонентов.
-        s = s.replace(f'{self.cd_path}/{self.JAR_REL_PATH}', '%JAR_PATH%')
-
-        open(circtmpl_path, 'w+').write(s)
-
-    def circ(self, circtmpl_path, circ_path: str):
+    def circ(self):
         """
         Создание .circ файла из .circtmpl с заменой всех шаблонов.
         """
-        s = open(circtmpl_path, 'r').read()
+        s = open(self.CIRCTMPL_NAME, 'r').read()
+        open(self.CIRC_NAME, 'w+').write(s.replace('%CD_PATH%', self.cd_path))
 
-        # Замена пути к микрокоду.
-        s = s.replace('%MICROCODE_PATH%', f'{self.cd_path}/{self.MICROCODE_REL_PATH}')
-        # Замена пути к JAR компонентов.
-        s = s.replace('%JAR_PATH%', f'{self.cd_path}/{self.JAR_REL_PATH}')
 
-        open(circ_path, 'w+').write(s)
+def main():
+    cd_path = os.getcwd()
+    modules = [
+        Logisim(cd_path)
+    ]
+
+    for target in sys.argv[1:]:
+        found = False
+        for module in modules:
+            if module.try_exec(target):
+                found = True
+                break
+
+        if not found:
+            Module.errors_count += 1
+            print(f"Target '{target}' not found.")
+
+    Module.report()
 
 
 if __name__ == "__main__":
-    # TODO: добавить функции, сделать обработку argv.
-    logi = Logisim(os.getcwd())
-    logi.circ('cpu.circtmpl', 'cpu.circ')
+    main()
